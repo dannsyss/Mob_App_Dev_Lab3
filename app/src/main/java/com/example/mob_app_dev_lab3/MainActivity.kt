ead53ae4d9f7fad30d97a7d3da74dcfaf958b53c
DanSys <danilasysolatin.ed@gmail.com>
2023-03-25 19:02:37 +0600

    Created markup with FrameLayout; Made a handler for ImageButton.

--- app/src/main/java/com/example/mob_app_dev_lab3/MainActivity.kt
+++ app/src/main/java/com/example/mob_app_dev_lab3/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.mob_app_dev_lab3
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.mob_app_dev_lab3.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding.imageView.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.cat_1))
+        val v:View= binding.root
+        binding.imageButton.setOnClickListener{
+            var rnd:Int=(1..3).random()
+            var str:String="cat_"+rnd.toString()
+
+            var resId:Int = getResources().getIdentifier(str, "drawable", getPackageName());
+
+            binding.imageView.setImageDrawable(ContextCompat.getDrawable(this,resId))
+        }
+
+        setContentView(v)
+
     }
 }
